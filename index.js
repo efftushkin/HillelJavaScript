80bf3eef6018821a2a6932fddad426b799d24fc7
sergey.kuchmiy <sergey.kuchmiy@ringcentral.com>
2024-05-22 01:26:48 +0300

    index.js after using ESLint

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-const ten = 10;
-const eleven = 11;
-
-console.log("The sum of", ten, "and", eleven, "is", sum(ten, eleven));
-console.log(ten, "minus", eleven, "is", subtract(ten, eleven));
-console.log(eleven, "times", ten, "is", multiply(ten, eleven));
-
 function sum(p1, p2) {
   return p1 + p2;
 }
@@ -16,3 +9,10 @@ function subtract(p1, p2) {
 function multiply(p1, p2) {
   return p1 * p2;
 }
+
+const ten = 10;
+const eleven = 11;
+
+console.log('The sum of', ten, 'and', eleven, 'is', sum(ten, eleven));
+console.log(ten, 'minus', eleven, 'is', subtract(ten, eleven));
+console.log(eleven, 'times', ten, 'is', multiply(ten, eleven));
