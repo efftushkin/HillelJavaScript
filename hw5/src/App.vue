17f7dc89a13c2c5f9a4bcb73e1e4d0a8109a8471
sergey.kuchmiy <sergey.kuchmiy@ringcentral.com>
2024-05-26 02:01:50 +0300

    Home Work 5 - added Card component

--- hw5/src/App.vue
+++ hw5/src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <card-hw5/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CardHw5 from './components/Card.vue'
 
 export default {
-  name: 'App',
+  name: 'HomeWork 5',
   components: {
-    HelloWorld
+    CardHw5
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
