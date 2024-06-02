7e7535dfe95bd16561e672145bd324e0b73f0626
sergey.kuchmiy <sergey.kuchmiy@ringcentral.com>
2024-06-02 19:22:20 +0300

    Init new branch with nested Vue project 'hw9'

---
+++ hw9/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <progress-hw9 :percentage='0'/>
+  <br>
+  <progress-hw9 :percentage='20'/>
+  <br>
+  <progress-hw9 :percentage='40'/>
+  <br>
+  <progress-hw9 :percentage='60'/>
+  <br>
+  <progress-hw9 :percentage='80'/>
+  <br>
+  <progress-hw9 :percentage='100'/>
+</template>
+
+<script>
+import ProgressHw9 from './components/Progress.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ProgressHw9
+  }
+}
+</script>
