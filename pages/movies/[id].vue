c0660bbf3f76f1e48f7b6663682081faedc31d89
sergey.kuchmiy <sergey.kuchmiy@ringcentral.com>
2024-08-09 18:46:22 +0300

    HW20 MovieFinder - Single Movie page with no data - first try

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,21 +1,49 @@
 <template>
   <div>
-    <h1>Single movie page {{route.params.id}}</h1>
+    <h1>{{movie.Title}}</h1>
+    <img :src="movie.Poster" :alt="`${movie.Title} poster`">
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, assumenda eum illo incidunt libero magni nemo officiis praesentium quam ratione saepe similique veritatis voluptate voluptatibus?
+      {{movie.Plot}}
     </p>
   </div>
 </template>
 
 <script setup>
+import {onMounted} from "vue";
 
 definePageMeta({
    layout: 'single-movie'
  })
 
 const route = useRoute()
+const movie = ref([{Title: 'Title for ' + route.params.id, Plot: 'Plot for ' + route.params.id, Poster: ''}])
 
+onMounted(() => {
+  getMovie()
+})
 
+const getMovie = async () => {
+  const data = await useFetch('http://www.omdbapi.com/?apikey=21eac196&i=' + route.params.id)
+
+  console.log(data)
+
+  if(data.data?.value?.Response === 'False') {
+    throw createError({
+      statusCode: 404,
+      statusMessage: data.data.value.Error,
+      fatal: true
+    })
+  } else if(data.error.value) {
+    throw createError({
+      statusCode: data.error.value.statusCode,
+      statusMessage: data.error.value.statusMessage,
+      fatal: true
+    })
+  } else {
+    movie.value = (data.data.value)
+  }
+  return data
+}
 
 </script>
 
